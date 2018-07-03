f6a338cf5c3ac3ac072fe1f714cc3b4f8db5ff4e
flukehn <limfc@qq.com>
2018-07-03 19:46:46 +0800

    233

--- gery.js
+++ gery.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
+	var cc=0;
 	$("#gery").click(function(){
-		document.getElementById('gery').innerHTML='<img src="images/gerysj.jpg" id="gery" width="30%" alt="Gery冒金光" />';
-		document.getElementById('nb').innerHTML='妙妙妙';
+		++cc;
+		if(cc>=3){
+			document.getElementById('gery').innerHTML='<img src="images/gerysj.jpg" id="gery" width="30%" alt="Gery冒金光" />';
+			document.getElementById('nb').innerHTML='妙妙妙';
+		}
 	});
 });
